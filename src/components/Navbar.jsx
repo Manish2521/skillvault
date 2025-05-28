0e8d83705ba12f0d062c3675acf7769307b70dd9
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-05-29 00:12:50 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -57,18 +57,18 @@ export default function Navbar({ token, setToken }) {
               <>
               
               <span className="inline-flex items-center gap-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-600 text-gray-800 dark:text-gray-100 font-medium px-2 py-2 rounded-xl shadow-sm">
-                <User className="w-5 h-5 text-blue-600 dark:text-blue-400" />
+{/*                 <User className="w-5 h-5 text-blue-600 dark:text-blue-400" /> */}
                 Hello, <span className="font-semibold text-blue-600 dark:text-blue-400">{userName.split(' ')[0]}</span>
               </span>
 
                 <button
                   onClick={confirmLogout}
                   className="flex items-center gap-1 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none 
-                  focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center 
+                  focus:ring-red-300 font-medium rounded-lg text-sm px-3 py-3 text-center 
                   dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-800"
                 >
                   <FaSignOutAlt />
-                  Logout
+                  
                 </button>
               </>
             ) : (
