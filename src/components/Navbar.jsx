da37f9ac4276ee1f02f5c3a4ed499507a013ee16
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-05-29 00:16:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -76,7 +76,7 @@ export default function Navbar({ token, setToken }) {
                 <Link
                   to="/login"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none 
-                  focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center 
+                  focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center 
                   dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center gap-1"
                 >
                   <FaSignInAlt /> Login
@@ -84,7 +84,7 @@ export default function Navbar({ token, setToken }) {
                 <Link
                   to="/signup"
                   className="text-white bg-green-600 hover:bg-green-700 focus:ring-4 focus:outline-none 
-                  focus:ring-green-300 font-medium rounded-lg text-sm px-4 py-2 text-center 
+                  focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center 
                   dark:bg-green-500 dark:hover:bg-green-600 dark:focus:ring-green-800 flex items-center gap-1"
                 >
                   <FaUserPlus /> Signup
