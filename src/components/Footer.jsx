2a77ea01d165e334b45ccfbf047e1226709396e3
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-05-29 14:38:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className='bg-gray-100 text-center py-4 text-sm text-gray-600 mt-10'>
+    <footer className='bg-gray-100 text-center py-4 text-sm text-gray-600'>
       © {new Date().getFullYear()} SkillVault. All rights reserved.
     </footer>
   )
