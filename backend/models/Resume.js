dc2403217482733d8163c76ae75110366037079f
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-05-31 17:51:46 +0530

    added file size and date created at

--- backend/models/Resume.js
+++ backend/models/Resume.js
@@ -5,6 +5,8 @@ const ResumeSchema = new mongoose.Schema(
     user:     { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     filename: { type: String, required: true },
     url:      { type: String, required: true },
+    sizeMB:   { type: Number, required: true },
+    createdAt:{ type: Date,   default: Date.now },
   },
   { timestamps: true }
 );
