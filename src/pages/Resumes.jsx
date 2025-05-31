2ef442f5b00a0955a0a1e4da8a1e880e2d692fcb
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-05-31 20:05:40 +0530

    Update Resumes.jsx

--- src/pages/Resumes.jsx
+++ src/pages/Resumes.jsx
@@ -91,7 +91,7 @@ export default function Resumes() {
             to="/add"
             className="bg-blue-600 hover:bg-blue-700 text-white font-medium px-4 py-2 rounded-md text-sm sm:text-base"
           >
-            + Add Resume
+            + Add Documents
           </Link>
         </div>
 
