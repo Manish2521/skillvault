a7cac15c1b5c1346c407f79d04abac978561a0e6
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2025-09-07 00:01:44 +0530

    removed localhost

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -26,7 +26,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/api/auth/google/callback",
+      callbackURL: "https://skillvault.netlify.app/api/auth/google/callback",
       passReqToCallback: true, 
     },
     async (req, accessToken, refreshToken, profile, done) => {
